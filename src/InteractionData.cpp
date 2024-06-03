2457ce223d26cade91b0d7519a57659f08405f6a
nitish-nayak <felarof.nayak@gmail.com>
2024-06-03 15:23:43 -0500

    remove redundant code

--- src/InteractionData.cpp
+++ src/InteractionData.cpp
@@ -60,7 +60,7 @@ namespace NeutrinoFluxReweight{
 
     InteractionData::Pt = Prod_P*sin_theta;
     InteractionData::Pz = Prod_P*cos_theta; 
-    
+   
     if(Inc_pdg < 1000010020) Inc_Mass = particle->GetParticle(Inc_pdg)->Mass();
     else if(Inc_pdg == 1000010020) {Inc_Mass = 1.875;}
     else {Inc_Mass = 2.809;}
@@ -69,9 +69,6 @@ namespace NeutrinoFluxReweight{
     else if(Prod_pdg == 1000010020) {Prod_Mass = 1.875;}
     else {Prod_Mass = 2.809;}
      
-    InteractionData::Inc_Mass  = Inc_Mass; // particle->GetParticle(Inc_pdg)->Mass();
-    InteractionData::Prod_Mass = Prod_Mass; // particle->GetParticle(Prod_pdg)->Mass();
-
     static const double NUCLEON_MASS = (particle->GetParticle(2112)->Mass() + particle->GetParticle(2212)->Mass())/2.;
     static const double NUCLEON_MASS2 = NUCLEON_MASS*NUCLEON_MASS;
 
